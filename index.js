4455c94e15a5a485dcc8e03b2fed3cde37470736
Jussi Pohjolainen <jussi.pohjolainen@gmail.com>
2021-02-23 13:10:05 +0200

    Lab 10: HTTP Post and validation using middleware

--- index.js
+++ index.js
@@ -4,9 +4,38 @@ const mung = require("express-mung");
 const locationrouter = require("./routes/locations.js");
 
 const app = express();
+
 app.use(express.static("public"));
 
 app.use(express.json());
+
+const validate = (loc) => {
+  if (!loc) return false;
+  if (!loc.hasOwnProperty("latitude") || !loc.hasOwnProperty("longitude"))
+    return false;
+
+  if (isNaN(Number(loc.latitude)) || isNaN(Number(loc.longitude))) return false;
+
+  return (
+    loc.latitude >= -90 &&
+    loc.latitude <= 90 &&
+    loc.longitude >= -180 &&
+    loc.longitude <= 180
+  );
+};
+
+app.use((req, res, next) => {
+  if (req.method === "POST") {
+    let location = req.body;
+    if (!validate(location)) {
+      res.status(400).end();
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+});
 
 app.use(
   mung.json((body, req, res) => {
--- routes/locations.js
+++ routes/locations.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const locationrouter = express.Router();
+const app = express();
 
 const database = require("../database/crudrepository.js");
 
